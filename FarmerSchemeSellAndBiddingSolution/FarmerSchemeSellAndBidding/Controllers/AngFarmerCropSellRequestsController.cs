6a3a675db9829f81bd63e332011bd8876830c9fd
ghanshyampatel1997 <ghanshyampatel1997@gmail.com>
2020-11-15 16:19:34 +0530

    Project WebApi Added

---
+++ FarmerSchemeSellAndBiddingSolution/FarmerSchemeSellAndBidding/Controllers/AngFarmerCropSellRequestsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FarmerSchemeSellAndBidding.Models;
+
+namespace FarmerSchemeSellAndBidding.Controllers
+{
+    public class AngFarmerCropSellRequestsController : ApiController
+    {
+        private FarmerSchemeDBEntities db = new FarmerSchemeDBEntities();
+
+        // POST: api/AngFarmerCropSellRequests
+        [ResponseType(typeof(AngFarmerCropSellRequest))]
+        public IHttpActionResult PostAngFarmerCropSellRequest(AngFarmerCropSellRequest angFarmerCropSellRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            int cid = (from cr in db.Cropprices
+                       where cr.cropname == angFarmerCropSellRequest.cropname
+                       select cr.CropId).First();
+            
+            FarmerCropdetil farmerCropdetil = new FarmerCropdetil();
+            
+            farmerCropdetil.cropType = angFarmerCropSellRequest.cropType;
+            farmerCropdetil.CropId = cid;
+            farmerCropdetil.fertilizerType = angFarmerCropSellRequest.fertilizerType;
+            farmerCropdetil.quantity = angFarmerCropSellRequest.quantity;
+            farmerCropdetil.SoilPHCertificate = angFarmerCropSellRequest.SoilPHCertificate;
+
+            db.FarmerCropdetils.Add(farmerCropdetil);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+       
+    }
+}
